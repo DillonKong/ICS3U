a944b79fe5a9194b8487dc9950fb5b16d5c3c543
Owner <Owner@10.17.192.100>
2016-03-02 14:59:50 -0400

    I added more exclamation marks

--- ICS3U/src/kong/HelloWold.java
+++ ICS3U/src/kong/HelloWold.java
@@ -1,13 +1,13 @@
 package kong;
 /* hellowoorld.java
  * This program will print "Hello World!" to the console
- * Dillon Kong
+ * Dillon Kong 
  * 2/3/16
  */
 public class HelloWold {
 
 	public static void main(String[] args) {
-		System.out.println ("Hello World!");
+		System.out.println ("Hello World!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 
 	}
 
