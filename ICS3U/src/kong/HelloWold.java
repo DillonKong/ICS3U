1e21507f1fdeda961056da7449ced34f09e7eeb3
Owner <Owner@10.17.192.100>
2016-03-02 14:47:01 -0400

    Hello

---
+++ ICS3U/src/kong/HelloWold.java
@@ -0,0 +1,14 @@
+package kong;
+/* hellowoorld.java
+ * This program will print "Hello World!" to the console
+ * Dillon Kong
+ * 2/3/16
+ */
+public class HelloWold {
+
+	public static void main(String[] args) {
+		System.out.println ("Hello World!");
+
+	}
+
+}
