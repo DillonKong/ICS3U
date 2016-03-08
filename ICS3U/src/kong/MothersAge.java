4b72e3f3704af40ecdf94f7b64edf88f9b8368e3
Owner <Owner@DELL>
2016-03-08 19:33:32 -0400

    Mothers age when the child was born

---
+++ ICS3U/src/kong/MothersAge.java
@@ -0,0 +1,36 @@
+package kong;
+
+import java.util.Scanner;
+
+public class MothersAge {
+	/*MothersAge
+	 * Inputs mother's, child's and both their ages. Then out put the mothers age when she gave birth
+	 * 8/3/16
+	 * Dillon Kong
+	 */
+	public static void main(String[] args) {
+		//Set up scanner for user input
+		Scanner scan = new Scanner (System.in);
+		//Line of questions for user and puts into variables
+		System.out.println("Whats's is the mothers name?");
+		String mothersName = scan.nextLine();
+		System.out.println("What's the mothers year of birth?");
+		int mothersAge =  Integer.parseInt(scan.nextLine());
+		System.out.println("Whats's is the childs name?");
+		String childsName =  scan.nextLine();
+		System.out.println("What's the childs year of birth?");
+		int childsAge =  Integer.parseInt(scan.nextLine());
+		//If statement for if the mothers age is inputed as smaller than the childs
+		if (mothersAge < childsAge)
+		{
+			System.out.println("That's not possible");
+		}
+		else 
+		{
+			//Calculats and outputs the mothers age
+			int motherAgeAtBirth = mothersAge-childsAge;
+			System.out.println(mothersName + " was " + motherAgeAtBirth + " when " + childsName + " was born.");
+		}
+	}
+
+}
---
+++ ICS3U/src/kong/NumberOutput.java
@@ -0,0 +1,23 @@
+package kong;
+
+public class NumberOutput {
+/* NumberOutput
+ * The program outputs differnt numbers and strings
+ * 7/3/16
+ * Dillon Kong
+ */
+	public static void main(String[] args) {
+	
+		System.out.println(489);
+		System.out.println("0");
+		System.out.println(-44);
+		System.out.println("4 + 3");
+		System.out.println(4+3);
+		System.out.println("38-9+25");
+		System.out.println(38-9+25);
+		System.out.println("26"+ "18");
+		System.out.println("26" + 18);
+		System.out.println(15 + "31");
+	}
+
+}
