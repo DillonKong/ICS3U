cf1104d2a14d1d7073d6abaf2d35b2ceddab5e06
Owner <Owner@192.168.0.14>
2016-05-15 17:56:33 -0300

    Tic Tac Toe and Word Guess Game

--- ICS3U/src/kong/TicTacToe.java
+++ ICS3U/src/kong/TicTacToe.java
@@ -5,115 +5,335 @@ import java.util.Scanner;
 public class TicTacToe {
 
 	public static void main(String[] args) {
+
 		@SuppressWarnings("resource")
 		Scanner scan = new Scanner (System.in);
-		char [] [] xAndO = { {' ' , ' ' , ' '} , {' ' , ' ' , ' '} , {' ' , ' ' , ' '} };
-		int row, column;
-		boolean win = false;
+		boolean play = true;
 
-		while (win == false)
-		{
-			if ((xAndO [0] [0] == xAndO [0] [1]) && (xAndO [0] [1] == xAndO [0] [2]) &&  xAndO [0] [0] != ' ')
+		while (play == true) {
+			//Initializes 2D array, 3 by 3 square
+			String [] [] xAndO = { {" " , " " , " "} , {" " , " " , " "} , {" " , " " , " "}};
+			//Initialize variables
+			int row = 0, column = 0;
+			boolean win = false;
+			String playAgain = null;
+
+			// Loop that'll run until there is a winner (win == false)
+			while (win == false)
 			{
-				win = true;
-				if (xAndO [0] [0] == 'X')
+				//Checks columns for winner. If true will set win to true
+				if (xAndO [0] [0] == xAndO [0] [1] && xAndO [0] [1] == xAndO [0] [2] &&  xAndO [0] [0] != " ")
+				{	
+					//Sets win to true to exit loop
+					win = true;
+					//Checks which player wins and prints the winner 
+					if (xAndO [0] [0] == "X")
+						System.out.println("Congradulations player X you win"); 
+					else
+						System.out.println("Congradulations player O you win");
+					System.out.println("Would you like to play again? (Yes or No)");
+					playAgain = scan.nextLine();
+					if (playAgain.equalsIgnoreCase("yes"))
+						play = true;
+					else if (playAgain.equalsIgnoreCase("no"))
+						play = false;
+				}
+				else if (xAndO [1] [0] == xAndO [1] [1] && xAndO [1] [1] == xAndO [1] [2] && xAndO [1] [0] != " " )
+				{	
+					//Sets win to true to exit loop
+					win = true;
+					//Checks which player wins and prints the winner 
+					if (xAndO [0] [0] == "X")
+						System.out.println("Congradulations player X you win");
+					else
+						System.out.println("Congradulations player O you win");
+					System.out.println("Would you like to play again? (Yes or No)");
+					playAgain = scan.nextLine();
+					if (playAgain.equalsIgnoreCase("yes"))
+						play = true;
+					else if (playAgain.equalsIgnoreCase("no"))
+						play = false;
+				}
+				else if (xAndO [2] [0] == xAndO [2] [1] && xAndO [2] [1] == xAndO [2] [2] && xAndO [2] [0] != " ")
+				{	
+					//Sets win to true to exit loop
+					win = true;
+					//Checks which player wins and prints the winner 
+					if (xAndO [0] [0] == "X")
+						System.out.println("Congradulations player X you win");
+					else
+						System.out.println("Congradulations player O you win");
+					System.out.println("Would you like to play again? (Yes or No)");
+					playAgain = scan.nextLine();
+					if (playAgain.equalsIgnoreCase("yes"))
+						play = true;
+					else if (playAgain.equalsIgnoreCase("no"))
+						play = false;
+				}
+				//Checks rows for winner. If true will set win to true 
+				else if (xAndO [0] [0] == xAndO [1] [0] && xAndO [1] [0] == xAndO [2] [0] && xAndO [0] [0] != " " )
+				{	
+					//Sets win to true to exit loop
+					win = true;
+					//Checks which player wins and prints the winner 
+					if (xAndO [0] [0] == "X")
+						System.out.println("Congradulations player X you win");
+					else
+						System.out.println("Congradulations player O you win");
+					System.out.println("Would you like to play again? (Yes or No)");
+					playAgain = scan.nextLine();
+					if (playAgain.equalsIgnoreCase("yes"))
+						play = true;
+					else if (playAgain.equalsIgnoreCase("no"))
+						play = false;
+				}
+				else if (xAndO [0] [1] == xAndO [1] [1] && xAndO [1] [1] == xAndO [2] [1] && xAndO [0] [1] != " " )
+				{	
+					//Sets win to true to exit loop
+					win = true;
+					//Checks which player wins and prints the winner 
+					if (xAndO [0] [0] == "X")
+						System.out.println("Congradulations player X you win");
+					else
+						System.out.println("Congradulations player O you win");
+					System.out.println("Would you like to play again? (Yes or No)");
+					playAgain = scan.nextLine();
+					if (playAgain.equalsIgnoreCase("yes"))
+						play = true;
+					else if (playAgain.equalsIgnoreCase("no"))
+						play = false;
+				}
+				else if (xAndO [0] [2] == xAndO [1] [2] && xAndO [1] [2] == xAndO [2] [2] && xAndO [0] [2] != " " )
+				{
+					//Sets win to true to exit loop
+					win = true;
+					//Checks which player wins and prints the winner 
+					if (xAndO [0] [2] == "X")
+						System.out.println("Congradulations player one you win");
+					else
+						System.out.println("Congradulations player two you win");
+					System.out.println("Would you like to play again? (Yes or No)");
+					playAgain = scan.nextLine();
+					if (playAgain.equalsIgnoreCase("yes"))
+						play = true;
+					else if (playAgain.equalsIgnoreCase("no"))
+						play = false;
+				}
+				//Checks the diagonals for winner. If true will set win to true
+				else if (xAndO [0] [0] == xAndO [1] [1] && xAndO [1] [1] == xAndO [2] [2] && xAndO [0] [0] != " " )
+				{	
+					//Sets win to true to exit loop
+					win = true;
+					//Checks which player wins and prints the winner 
+					if (xAndO [0] [0] == "X")
+						System.out.println("Congradulations player X you win");
+					else
+						System.out.println("Congradulations player O you win");
+					System.out.println("Would you like to play again? (Yes or No)");
+					playAgain = scan.nextLine();
+					if (playAgain.equalsIgnoreCase("yes"))
+						play = true;
+					else if (playAgain.equalsIgnoreCase("no"))
+						play = false;
+				}
+				else if (xAndO [0] [2] == xAndO [1] [1] && xAndO [1] [1] == xAndO [2] [0] && xAndO [0] [2] != " " )
+				{	
+					//Sets win to true to exit loop
+					win = true;
+					//Checks which player wins and prints the winner 
+					if (xAndO [0] [0] == "X")
+						System.out.println("Congradulations player X you win");
+					else
+						System.out.println("Congradulations player O you win");
+					System.out.println("Would you like to play again? (Yes or No)");
+					playAgain = scan.nextLine();
+					if (playAgain.equalsIgnoreCase("yes"))
+						play = true;
+					else if (playAgain.equalsIgnoreCase("no"))
+						play = false;
+				}
+				else if (xAndO [0] [0] != " " && xAndO [0] [1] != " " && xAndO [0] [2] != " "&& xAndO [1] [0] != " "&& xAndO [1] [1] != " "&& xAndO [1] [2] != " "&& xAndO [2] [0] != " "
+						&& xAndO [2] [1] != " "&& xAndO [2] [2] != " ")
+				{
+					win = true;
+					System.out.println("Cats game; there's no winner");
+				}
+				else 
+				{
+					//Asks for player X where they like to place an X
+					System.out.println("Player X. Where woud you like to go? Row first then column place. \n Row (1-3)" );
+					//Row is subtracted by 1 because in a array it starts at 0 not 1
+					row = scan.nextInt();
+					row --;
+					System.out.println("Column (1-3)");
+					//Column is subtracted by 1 because in a array it starts at 0 not 1
+					column = scan.nextInt();
+					column --;
+					xAndO [row] [column] = "X";
 
-					System.out.println("Congradulations player one you win");	
-				else
-					System.out.println("Congradulations player two you win");
-			}
-			else if (xAndO [1] [0] == xAndO [1] [1] && xAndO [1] [1] == xAndO [1] [2] && xAndO [1] [0] != ' ' )
-			{
-				win = true;
-				if (xAndO [1] [0] == 'X')
+					// Prints grid with updated X and O placement with grid numbers
+					System.out.println("   " + "1" + "   2 " + "   3");
+					System.out.println("1  " + xAndO [0] [0] + " | " + xAndO [0] [1] + " | " + xAndO [0] [2]);
+					System.out.println("  ------------");
+					System.out.println("2  " + xAndO [1] [0] + " | " + xAndO [1] [1] + " | " + xAndO [1] [2]);
+					System.out.println("  ------------");
+					System.out.println("3  " + xAndO [2] [0] + " | " + xAndO [2] [1] + " | " + xAndO [2] [2]);
 
-					System.out.println("Congradulations player one you win");	
-				else
-					System.out.println("Congradulations player two you win");
-			}
-			else if (xAndO [2] [0] == xAndO [2] [1] && xAndO [2] [1] == xAndO [2] [2] && xAndO [2] [0] != ' ')
-			{
-				win = true;
-				if (xAndO [2] [0] == 'X')
+					//Checks columns for winner. If true will set win to true
+					if (xAndO [0] [0] == xAndO [0] [1] && xAndO [0] [1] == xAndO [0] [2] &&  xAndO [0] [0] != " ")
+					{
+						win = true;
+						if (xAndO [0] [0] == "X")
+							System.out.println("Congradulations player one you win");
+						else
 
-					System.out.println("Congradulations player one you win");	
-				else
-					System.out.println("Congradulations player two you win");
-			}
+							System.out.println("Congradulations player two you win");
+						System.out.println("Would you like to play again? (Yes or No)");
+						playAgain = scan.nextLine();
+						if (playAgain.equalsIgnoreCase("yes"))
+							play = true;
+						else if (playAgain.equalsIgnoreCase("no"))
+							play = false;
+					}
+					else if (xAndO [1] [0] == xAndO [1] [1] && xAndO [1] [1] == xAndO [1] [2] && xAndO [1] [0] != " " )
+					{	
+						//Sets win to true to exit loop
+						win = true;
+						//Checks which player wins and prints the winner 
+						if (xAndO [0] [0] == "X")
+							System.out.println("Congradulations player X you win");
+						else
+							System.out.println("Congradulations player O you win");
+						System.out.println("Would you like to play again? (Yes or No)");
+						playAgain = scan.nextLine();
+						if (playAgain.equalsIgnoreCase("yes"))
+							play = true;
+						else if (playAgain.equalsIgnoreCase("no"))
+							play = false;
+					}
+					else if (xAndO [2] [0] == xAndO [2] [1] && xAndO [2] [1] == xAndO [2] [2] && xAndO [2] [0] != " ")
+					{	
+						//Sets win to true to exit loop
+						win = true;
+						//Checks which player wins and prints the winner 
+						if (xAndO [0] [0] == "X")
+							System.out.println("Congradulations player X you win");
+						else
+							System.out.println("Congradulations player O you win");
+						System.out.println("Would you like to play again? (Yes or No)");
+						playAgain = scan.nextLine();
+						if (playAgain.equalsIgnoreCase("yes"))
+							play = true;
+						else if (playAgain.equalsIgnoreCase("no"))
+							play = false;
+					}
+					//Checks rows for winner. If true will set win to true
+					else if (xAndO [0] [0] == xAndO [1] [0] && xAndO [1] [0] == xAndO [2] [0] && xAndO [0] [0] != " " )
+					{
+						win = true;
+						if (xAndO [0] [0] == "X")
+							System.out.println("Congradulations player one you win");
+						else
+							System.out.println("Congradulations player two you win");
+						System.out.println("Would you like to play again? (Yes or No)");
+						playAgain = scan.nextLine();
+						if (playAgain.equalsIgnoreCase("yes"))
+							play = true;
+						else if (playAgain.equalsIgnoreCase("no"))
+							play = false;
+					}
+					else if (xAndO [0] [1] == xAndO [1] [1] && xAndO [1] [1] == xAndO [2] [1] && xAndO [0] [1] != " " )
+					{	
+						//Sets win to true to exit loop
+						win = true;
+						//Checks which player wins and prints the winner 
+						if (xAndO [0] [0] == "X")
+							System.out.println("Congradulations player X you win");
+						else
+							System.out.println("Congradulations player O you win");
+						System.out.println("Would you like to play again? (Yes or No)");
+						playAgain = scan.nextLine();
+						if (playAgain.equalsIgnoreCase("yes"))
+							play = true;
+						else if (playAgain.equalsIgnoreCase("no"))
+							play = false;
+					}
+					else if (xAndO [0] [2] == xAndO [1] [2] && xAndO [1] [2] == xAndO [2] [2] && xAndO [0] [2] != " " )
+					{	
+						//Sets win to true to exit loop
+						win = true;
+						//Checks which player wins and prints the winner 
+						if (xAndO [0] [0] == "X")
+							System.out.println("Congradulations player X you win");
+						else
+							System.out.println("Congradulations player O you win");
+						System.out.println("Would you like to play again? (Yes or No)");
+						playAgain = scan.nextLine();
+						if (playAgain.equalsIgnoreCase("yes"))
+							play = true;
+						else if (playAgain.equalsIgnoreCase("no"))
+							play = false;
+					}
+					//Checks diagonals for winner. If true will set win to true
+					else if (xAndO [0] [0] == xAndO [1] [1] && xAndO [1] [1] == xAndO [2] [2] && xAndO [0] [0] != " " )
+					{
+						win = true;
+						if (xAndO [0] [0] == "X")
+							System.out.println("Congradulations player one you win");
+						else
+							System.out.println("Congradulations player two you win");
+						System.out.println("Would you like to play again? (Yes or No)");
+						playAgain = scan.nextLine();
+						if (playAgain.equalsIgnoreCase("yes"))
+							play = true;
+						else if (playAgain.equalsIgnoreCase("no"))
+							play = false;
+					}
+					else if (xAndO [0] [2] == xAndO [1] [1] && xAndO [1] [1] == xAndO [2] [0] && xAndO [0] [2] != " " )
+					{	
+						//Sets win to true to exit loop
+						win = true;
+						//Checks which player wins and prints the winner 
+						if (xAndO [0] [0] == "X")
+							System.out.println("Congradulations player X you win");
+						else
+							System.out.println("Congradulations player O you win");
+						System.out.println("Would you like to play again? (Yes or No)");
+						playAgain = scan.nextLine();
+						if (playAgain.equalsIgnoreCase("yes"))
+							play = true;
+						else if (playAgain.equalsIgnoreCase("no"))
+							play = false;
+					}
+					else if (xAndO [0] [0] != " " && xAndO [0] [1] != " " && xAndO [0] [2] != " "&& xAndO [1] [0] != " "&& xAndO [1] [1] != " "&& xAndO [1] [2] != " "&& xAndO [2] [0] != " "
+							&& xAndO [2] [1] != " "&& xAndO [2] [2] != " ")
+					{
+						win = true;
+						System.out.println("Cats game; there's no winner");
+					}
+					else
+					{
+						//Asks for player X where they like to place an X
+						System.out.println("Player 2, you're Os. Where woud you like to go? Row first then column place. \n Row (1-3)" );
+						//Row is subtracted by 1 because in a array it starts at 0 not 1 
+						row = scan.nextInt();
+						row --;
+						System.out.println("Column (1-3)");
+						//Column is subtracted by 1 because in a array it starts at 0 not 1
+						column = scan.nextInt();
+						column --;
+						xAndO [row] [column] = "O";
 
-			else if (xAndO [0] [0] == xAndO [1] [0] && xAndO [1] [0] == xAndO [1] [1] && xAndO [0] [0] != ' ' )
-			{
-				win = true;
-				if (xAndO [0] [0] == 'X')
-
-					System.out.println("Congradulations player one you win");	
-				else
-					System.out.println("Congradulations player two you win");
-			}
-			else if (xAndO [0] [1] == xAndO [1] [1] && xAndO [1] [1] == xAndO [1] [2] && xAndO [0] [1] != ' ' )
-			{
-				win = true;
-				if (xAndO [0] [1] == 'X')
-
-					System.out.println("Congradulations player one you win");	
-				else
-					System.out.println("Congradulations player two you win");
-			}
-			else if (xAndO [0] [2] == xAndO [1] [2] && xAndO [1] [2] == xAndO [2] [2] && xAndO [0] [2] != ' ' )
-			{
-				win = true;
-				if (xAndO [0] [2] == 'X')
-
-					System.out.println("Congradulations player one you win");	
-				else
-					System.out.println("Congradulations player two you win");
-			}
-
-			else if (xAndO [0] [0] == xAndO [1] [1] && xAndO [1] [1] == xAndO [2] [2] && xAndO [0] [0] != ' ' )
-			{
-				win = true;
-				if (xAndO [0] [0] == 'X')
-
-					System.out.println("Congradulations player one you win");	
-				else
-					System.out.println("Congradulations player two you win");
-			}
-			else if (xAndO [0] [2] == xAndO [1] [1] && xAndO [1] [1] == xAndO [2] [0] && xAndO [0] [2] != ' ' )
-			{
-				win = true;
-				if (xAndO [0] [2] == 'X')
-
-					System.out.println("Congradulations player one you win");	
-				else
-					System.out.println("Congradulations player two you win");
-			}
-
-			else {
-
-				System.out.println("Player 1, you're Xs. Where woud you like to go? Row first then column place. \n Row (0-2)" );
-				row = scan.nextInt();
-				System.out.println("Column (0-2)");
-				column = scan.nextInt();
-				xAndO [row] [column] = 'X';
-
-				System.out.println(xAndO [0] [0] + " | " + xAndO [0] [1] + " | " + xAndO [0] [2]);
-				System.out.println("----------");
-				System.out.println(xAndO [1] [0] + " | " + xAndO [1] [1] + " | " + xAndO [1] [2]);
-				System.out.println("----------");
-				System.out.println(xAndO [2] [0] + " | " + xAndO [2] [1] + " | " + xAndO [2] [2]);
-
-				System.out.println("Player 1, you're Os. Where woud you like to go? Row first then column place. \n Row (0-2)" );
-				row = scan.nextInt();
-				System.out.println("Column (0-2)");
-				column = scan.nextInt();
-				xAndO [row] [column] = 'O';
-				System.out.println(xAndO [0] [0] + " | " + xAndO [0] [1] + " | " + xAndO [0] [2]);
-				System.out.println("----------");
-				System.out.println(xAndO [1] [0] + " | " + xAndO [1] [1] + " | " + xAndO [1] [2]);
-				System.out.println("----------");
-				System.out.println(xAndO [2] [0] + " | " + xAndO [2] [1] + " | " + xAndO [2] [2]);
+						// Prints grid with updated X and O placement with grid numbers
+						System.out.println("   " + "1" + "   2 " + "   3");
+						System.out.println("1  " + xAndO [0] [0] + " | " + xAndO [0] [1] + " | " + xAndO [0] [2]);
+						System.out.println("  ------------");
+						System.out.println("2  " + xAndO [1] [0] + " | " + xAndO [1] [1] + " | " + xAndO [1] [2]);
+						System.out.println("  ------------");
+						System.out.println("3  " + xAndO [2] [0] + " | " + xAndO [2] [1] + " | " + xAndO [2] [2]);
+					}
+				}
 			}
 		}
 	}
-
 }
--- ICS3U/src/kong/WordGuess1.java
+++ ICS3U/src/kong/WordGuess1.java
@@ -5,72 +5,75 @@ import java.util.Scanner;
 public class WordGuess1 {
 
 	public static void main(String[] args) {
-
-		String[] secretWords = new String[]{"Brain" , "Tree" , "Happy" , "Computer" , "Phone"};
+		// Initialize all variables
+		String[] secretWords = new String[]{"brain", "tree" , "happy" , "computer" , "phone", "technology" , "charger" , "cars" , "music" , "screen" , "haircut" , "programming"};
 		@SuppressWarnings("resource")
 		Scanner read = new Scanner(System.in);
-		int randomWord = (int) (Math.random() * 5);
-		boolean dashes = true;
-		String userGuess = "#";
-		String guessWord;
+		int randomWord = (int) (Math.random() * secretWords.length), counter = 0;
+		boolean incomplete = true;
+		String userGuess = "#", guessWord;
 		char[] wordDashes = new char[secretWords[randomWord].length()];
-		System.out.println("Word Guessing Game...");
+		char[] wordOfsecretWords = secretWords[randomWord].toCharArray();
 
+		System.out.println("Word Guessing Game");
+
+		//Prints dashes for the random word
 		for (int i = 0; i < secretWords[randomWord].length(); i++) 
 			wordDashes[i] = '-';
 
-
-		char[] wordOfsecretWords = secretWords[randomWord].toCharArray();
-
-
-		while (!userGuess.equalsIgnoreCase("!") && dashes) 
+		//User guesses letters until ! is inputed
+		while (!userGuess.equalsIgnoreCase("!") && incomplete == true)
 		{
-
-			for(int i = 0; i < wordDashes.length; i++)
+			for(int i = 0; i < wordDashes.length; i++)	
 			{
 				if(wordOfsecretWords[i] == userGuess.charAt(0))
 					wordDashes[i] = userGuess.charAt(0);
-				System.out.print(wordDashes[i]);
 
+				System.out.print(wordDashes[i]);
 			}
 
 			for (int i = 0; i < wordDashes.length; i++) 
 			{
 				if (wordDashes[i] == '-') 
 				{
-					dashes = true;
+					incomplete = true;
 					break;
 				}
 				if (wordDashes[i] != '-') 
-					dashes = false;
+					incomplete = false;
 			}
 			System.out.println();
-			if(dashes){
+			if(incomplete)
+			{
 				System.out.print("Enter a letter (or a ! to guess the word):");
-
+				counter ++;
 				userGuess = read.nextLine();
 			}
 		}
-
-		if (!dashes) {
+		// If the word is guessed incompletely, win, the incomplete word and the number of guesses if printed
+		if (!incomplete)
+		{
 			System.out.println("You win!");
 			System.out.println("You guessed the word, it was: " + "'" + secretWords[randomWord] + "'");
+			System.out.println("You took " + counter + " guesses to get it correct");
 		} 
-
 		else 
 		{
 			System.out.println("What is your guess?");
 			guessWord = read.nextLine();
+			counter ++;
 
 			if(guessWord.equalsIgnoreCase(secretWords[randomWord]))
 			{
 				System.out.println("Congrats, you won!");
 				System.out.println("You guessed the word, it was: " + "'" + secretWords[randomWord] + "'");
+				System.out.println("You took " + counter + " guesses to get it correct");
 			}
 			else
 			{
 				System.out.println("You failed the task.");
 				System.out.println("You did not guess the word, it was: " + "'" + secretWords[randomWord] + "'");
+				System.out.println("You took " + counter + " guesses to get it correct");
 			}
 		}
 	}
