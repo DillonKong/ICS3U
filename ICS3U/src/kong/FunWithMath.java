c9b75fca28b0dade1d95ad65fd5b78da99fbea15
Owner <Owner@192.168.0.13>
2016-05-24 19:16:06 -0300

    Fun with numbers 1 and 2

---
+++ ICS3U/src/kong/FunWithMath.java
@@ -0,0 +1,44 @@
+package kong;
+
+import java.util.Scanner;
+
+public class FunWithMath {
+
+		public static  int numOne = 0, numTwo = 0, total = 0;
+		static Scanner scan = new Scanner (System.in);
+
+		public static void main(String[] args) {
+			getNumbers();
+			add();
+			subtract();
+			multiply();
+			divide();
+		}
+		
+		public static void getNumbers(){
+			System.out.println("What's the first number?");
+			numOne = scan.nextInt();
+			System.out.println("What's the second number?");
+			numTwo = scan.nextInt();
+		}
+		
+		public static void add(){
+			total = numOne + numTwo;
+			System.out.println(total);
+		}
+		
+		public static void subtract(){
+			total = numOne - numTwo;
+			System.out.println(total);
+		}
+		
+		public static void multiply(){
+			total = numOne * numTwo;
+			System.out.println(total);
+		}
+		
+		public static void divide(){
+			total = numOne / numTwo;
+			System.out.println(total);
+		}
+	}
--- ICS3U/src/kong/FunWithMathTwo.java
+++ ICS3U/src/kong/FunWithMathTwo.java
@@ -2,42 +2,45 @@ package kong;
 
 import java.util.Scanner;
 
-public class twoNumberOperations {
+public class FunWithMathTwo {
+	
 	public static  int numOne = 0, numTwo = 0, total = 0;
 	static Scanner scan = new Scanner (System.in);
 
 	public static void main(String[] args) {
+
 		getNumbers();
 		add();
+		System.out.println(total);
 		subtract();
+		System.out.println(total);
 		multiply();
+		System.out.println(total);
 		divide();
+		System.out.println(total);
 	}
-	
+
 	public static void getNumbers(){
 		System.out.println("What's the first number?");
 		numOne = scan.nextInt();
 		System.out.println("What's the second number?");
 		numTwo = scan.nextInt();
 	}
-	
+
 	public static void add(){
 		total = numOne + numTwo;
-		System.out.println(total);
+		
 	}
-	
+
 	public static void subtract(){
 		total = numOne - numTwo;
-		System.out.println(total);
 	}
-	
+
 	public static void multiply(){
 		total = numOne * numTwo;
-		System.out.println(total);
 	}
-	
+
 	public static void divide(){
 		total = numOne / numTwo;
-		System.out.println(total);
 	}
 }
