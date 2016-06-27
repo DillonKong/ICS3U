3b91dd90a8b2597b3e039c5d0b944b12236837ff
Owner <Owner@192.168.0.11>
2016-06-27 14:07:15 -0300

    Tech culminating

--- Tech Culminating/src/test.java
+++ Tech Culminating/src/test.java
@@ -1,15 +1,9 @@
-
+import java.util.Scanner;
 
 public class test {
 
 	public static void main(String[] args) 
 	{
-		
-		for (int i = 0; i < 25; i ++)
-		{
-			int rndNum1 = (int) (Math.random() * 7) + 1;
-			System.out.println(rndNum1);
-		}
 	}
 
 }
